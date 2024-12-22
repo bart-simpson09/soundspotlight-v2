9520dfa1ff0cc863e4e213a6a6cc0a648530736e
Mateusz Sajdak <sajdak.mateusz.219@gmail.com>
2024-12-22 15:45:55 +0100

    Added albums in our library search area

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import NavBar from "../../components/NavBar";
 import { useLocation } from "react-router-dom";
 import {useDashboard} from "./UseDashboard";
+import {AlbumList, CompactDisc, Language, User} from "iconoir-react";
 
 export const Dashboard: React.FC = () => {
     let location = useLocation();
@@ -29,10 +30,69 @@ export const Dashboard: React.FC = () => {
 
     return (
         <>
-            <NavBar highlighted="home" />
+            <NavBar highlighted="home"/>
             <p>
                 {authors?.map(author => author.name).join(', ')}
             </p>
+
+            <div className="globalPageContainer flexColumn">
+                <div className="flexColumn rowGap24">
+                    <h1>Albums in our library</h1>
+
+                    <div className="flexRow searchBar columnGap16">
+
+                        <div className="inputArea flexColumn rowGap8">
+                            <label htmlFor="albumTitle">Album title</label>
+                            <div className="inputWithIcon">
+                                <input type="text" name="albumTitle" id="albumTitle" placeholder="Type album title"/>
+                                <CompactDisc className={"inputIcon"} />
+                            </div>
+                        </div>
+
+                        <div className="inputArea flexColumn rowGap8">
+                            <label htmlFor="artistName">Artist name</label>
+                            <div className="inputWithIcon">
+                                <input type="text" name="artistName" id="artistName" placeholder="Type artist name"/>
+                                <User className={"inputIcon"} />
+                            </div>
+                        </div>
+
+                        <div className="inputArea flexColumn rowGap8">
+                            <label htmlFor="category">Category</label>
+                            <div className="inputWithIcon">
+                                <div className="customSelect">
+                                    <select name="category" id="category">
+                                        <option value="0">All categories</option>
+                                        {/*<?php foreach ($categories as $category): ?>*/}
+                                        {/*<option*/}
+                                        {/*    value="<?= $category->getCategoryId() ?>"><?= $category->getCategoryName() ?></option>*/}
+                                        {/*<?php endforeach; ?>*/}
+                                    </select>
+                                </div>
+                                <AlbumList className={"inputIcon"} />
+                            </div>
+                        </div>
+
+                        <div className="inputArea flexColumn rowGap8">
+                            <label htmlFor="language">Language</label>
+                            <div className="inputWithIcon">
+                                <div className="customSelect">
+                                    <select name="language" id="language">
+                                        <option value="0">All languages</option>
+                                        {/*<?php foreach ($languages as $language): ?>*/}
+                                        {/*<option*/}
+                                        {/*    value="<?= $language->getLanguageId() ?>"><?= $language->getLanguageName() ?></option>*/}
+                                        {/*<?php endforeach; ?>*/}
+                                    </select>
+                                </div>
+                                <Language className={"inputIcon"} />
+                            </div>
+                        </div>
+
+                        <button className="buttonPrimary" id="searchButton">Search albums</button>
+                    </div>
+                </div>
+            </div>
 
         </>
     );
